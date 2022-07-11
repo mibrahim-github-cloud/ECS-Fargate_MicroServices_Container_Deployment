76bd9b8df205d8069680d31594ce1b105116c832
mibrahim-github-cloud <52443218+mibrahim-github-cloud@users.noreply.github.com>
2022-07-11 20:48:49 +0530

    Currency exchange service with RDS mysql
    Currency exchange service with RDS mysql

---
+++ currency-exchange-service-mysql/src/main/java/com/container/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationMySql.java
@@ -0,0 +1,35 @@
+package com.container.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.container.microservices.currencyexchangeservice.resource.ExchangeValue;
+import com.container.microservices.currencyexchangeservice.resource.ExchangeValueRepository;
+
+@SpringBootApplication
+public class CurrencyExchangeServiceApplicationMySql implements CommandLineRunner {
+
+	@Autowired
+	ExchangeValueRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplicationMySql.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		long count = repository.count();
+
+		if (count == 0) {
+			repository.save(new ExchangeValue(10001L, "USD", "INR", BigDecimal.valueOf(60)));
+			repository.save(new ExchangeValue(10002L, "EUR", "INR", BigDecimal.valueOf(70)));
+			repository.save(new ExchangeValue(10003L, "AUD", "INR", BigDecimal.valueOf(20)));
+		}
+	}
+
+}
